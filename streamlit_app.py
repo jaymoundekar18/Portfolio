01526aabd79c30920b827ff48437114e964ef9e3
Jay Moundekar <60815668+jaymoundekar18@users.noreply.github.com>
2024-05-31 17:53:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,8 @@ st.write("JAY MOUNDEKAR")
 st.echo()
 with st.echo():
   st.write('Code will be executed and printed')
+
+st.sidebar.selectbox(
+    "How would you like to be contacted?",
+    ("Email", "Home phone", "Mobile phone")
+)
